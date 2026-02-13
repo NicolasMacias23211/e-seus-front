f57c70da6b2f720a432504d58af4d64f3fe5da6a
SEBASTIAN CAMILO JIMENEZ HERNANDEZ <sjimeher@emtelco.com.co>
2026-02-13 17:08:39 -0500

    Se separan el metodo getAll y el metodo getAllPagination, para ser utilizados segun el caso, y se agregar que la pagination resiva cuando elementos llegaron de la consulta

--- services/ansService.ts
+++ services/ansService.ts
@@ -5,7 +5,11 @@ import { http, type ApiResponse, type PaginatedResponse} from "./http";
 export class AnsService {
   private endpoint = "/ans/";
 
-  async getAll(page: number , pageSize: number): Promise<ApiResponse<PaginatedResponse<ANS>>> {
+  async getAll(): Promise<ApiResponse<PaginatedResponse<ANS>>> {
+    return await http.get<PaginatedResponse<ANS>>(this.endpoint);
+  }
+
+  async getAllPaginated(page: number , pageSize: number): Promise<ApiResponse<PaginatedResponse<ANS>>> {
     return await http.get<PaginatedResponse<ANS>>(
       `${this.endpoint}?page=${page}&page_size=${pageSize}`
     );
